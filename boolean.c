243dbe54e158acec2596e5aa1fb44df504c3c041
Robert Ellefson <noetic33@gmail.com>
2016-03-11 16:42:51 -0800

    modified boolean.c with two else if statements to determine ifa number is positive or zero

--- boolean.c
+++ boolean.c
@@ -6,7 +6,11 @@ int main()
  int b;
  printf("Enter a value:");
  scanf("%d", &b);
- if(b<0)
+ if(b == 0)
+   printf("The value is zero\n");
+ else if(b > 0)
+   printf("The value is positive\n");
+ else if(b < 0)
    printf("The value is negative\n");
  return 0;
 }
